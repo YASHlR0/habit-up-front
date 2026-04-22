242a7c803ea653f8a8c8338d84d9244d97bf3446
YASHlR0 <243841@ids.edu.upchiapas.edu.mx>
2026-04-22 00:58:01 -0600

    Corrigiendo endpoint de https a http para evitar error SSL

--- src/lib/api/apiBaseUrl.ts
+++ src/lib/api/apiBaseUrl.ts
@@ -3,5 +3,5 @@
  * Forzada a la IP de AWS con HTTP (sin la S) para evitar errores de SSL con el backend de Java.
  */
 export function getApiBaseUrl(): string {
-  return 'http://100.50.61.245:8080/api';
+return 'http://100.50.61.245:8080/api';
 }
